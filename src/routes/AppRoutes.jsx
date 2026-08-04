a19fb084bc6fcaaeb0fe6111b971b0886f2bf01c
Rishij4 <162426055+Rishij4@users.noreply.github.com>
2026-08-05 00:08:19 +0530

    Add Register route to AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -40,6 +40,7 @@ import AppointmentDetails from "../pages/appointments/AppointmentDetails";
 import EditAppointment from "../pages/appointments/EditAppointment";
 import EditDepartment from "../pages/departments/EditDepartment";
 import DepartmentDetails from "../pages/departments/DepartmentDetails";
+import Register from "./pages/auth/Register";
 export default function AppRoutes() {
   return (
     <Routes>
@@ -344,6 +345,7 @@ export default function AppRoutes() {
     </ProtectedRoute>
   }
 />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
